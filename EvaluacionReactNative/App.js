44667fde61bfa5fe621bb1cb8c76a222bd4f2bd8
SrAdrielM <20230542@ricaldone.edu.sv>
2025-09-01 14:48:33 -0600

    iconssss

--- EvaluacionReactNative/App.js
+++ EvaluacionReactNative/App.js
@@ -1,12 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
+import React, { useEffect } from "react";
 import Navigation from './src/navigation/Navigation';
-import { StyleSheet, Text, View } from 'react-native';
 import { AuthProvider } from './src/context/AuthContext';
+import * as SplashScreen from 'expo-splash-screen';
 
 export default function App() {
+  useEffect(() => {
+    const preparar = async () => {
+      try {
+        // Evita que se oculte automáticamente
+        await SplashScreen.preventAutoHideAsync();
+
+        // Simula carga de recursos, etc.
+        await new Promise(resolve => setTimeout(resolve, 3000)); // Puedes ajustar el tiempo
+
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Oculta el splash screen
+        await SplashScreen.hideAsync();
+      }
+    };
+
+    preparar();
+  }, []);
+
   return (
     <AuthProvider>
-          <Navigation/>
+      <Navigation />
     </AuthProvider>
   );
 }
