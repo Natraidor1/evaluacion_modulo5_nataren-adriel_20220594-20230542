97ee69bc2d928262e67ffc96b150e1b37e234b26
SrAdrielM <20230542@ricaldone.edu.sv>
2025-09-01 14:54:47 -0600

    splashscreen

--- EvaluacionReactNative/App.js
+++ EvaluacionReactNative/App.js
@@ -1,32 +1,25 @@
-import React, { useEffect } from "react";
+import React, {useEffect} from "react";
+import { StatusBar } from 'expo-status-bar';
 import Navigation from './src/navigation/Navigation';
+import { StyleSheet, Text, View } from 'react-native';
 import { AuthProvider } from './src/context/AuthContext';
 import * as SplashScreen from 'expo-splash-screen';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
   useEffect(() => {
     const preparar = async () => {
-      try {
-        // Evita que se oculte automáticamente
-        await SplashScreen.preventAutoHideAsync();
-
-        // Simula carga de recursos, etc.
-        await new Promise(resolve => setTimeout(resolve, 3000)); // Puedes ajustar el tiempo
-
-      } catch (e) {
-        console.warn(e);
-      } finally {
-        // Oculta el splash screen
-        await SplashScreen.hideAsync();
-      }
+      // Simula carga de recursos, API, etc.
+      await new Promise(resolve => setTimeout(resolve, 4000));
+      await SplashScreen.hideAsync();
     };
 
     preparar();
   }, []);
-
   return (
     <AuthProvider>
-      <Navigation />
+          <Navigation/>
     </AuthProvider>
   );
 }
