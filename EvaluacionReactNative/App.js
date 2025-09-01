d35ae3eab10f07a72339b17a380641c286d74441
unknown <20220594@ricaldone.edu.sv>
2025-09-01 09:51:07 -0600

    creacion ddel AuthContext para el manejo de login

--- EvaluacionReactNative/App.js
+++ EvaluacionReactNative/App.js
@@ -3,8 +3,6 @@ import Navigation from './src/navigation/Navigation';
 import { StyleSheet, Text, View } from 'react-native';
 import { AuthProvider } from './src/context/AuthContext';
 
-exp
-import Navigation from './src/navigation/Navigation';
 export default function App() {
   return (
     <AuthProvider>
